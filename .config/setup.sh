0a77bff7ece525658a9a045d9e75fd636da5ebd6
John Campbell <williejohn2003@yahoo.com>
2024-09-23 23:30:58 -0400

    initial commmit

---
+++ .config/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#this script will set up the environment to my liking
+#
+#screen resolution
+autorandr --change
+
+##screen brightness
+xrandr --output eDP-1 --brightness 1.5
+
+#reset terminal
+#clear
